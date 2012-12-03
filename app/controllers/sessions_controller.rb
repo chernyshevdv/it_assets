30a584bbd68963290975b9f07c96dd36a87aafc7
Dmitry Chernyshov <chernyshevdv@gmail.com>
2012-12-03 17:33:47 +0400

    Password change reminder if empty

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class SessionsController < ApplicationController
 	def new
 	end
@@ -6,7 +7,12 @@ class SessionsController < ApplicationController
 		user = Employee.find_by_login(params[:session][:login])
 		if user && (user.password_digest.nil? || user.authenticate(params[:session][:password]))
 			sign_in user
-			redirect_to current_user
+			if user.password_digest.nil?
+				flash[:notice] = 'Ваш пароль пуст! Это очень нехорошо. Настройте свой пароль!' 
+				redirect_to edit_employee_path(current_user)
+			else
+				redirect_to current_user
+			end
 		else
 			flash[:error] = 'Login or password is incorrect.'
 			redirect_to login_path
