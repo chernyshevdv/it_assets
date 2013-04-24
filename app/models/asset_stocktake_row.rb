9c5714f9c63b0b86f951296503d18b710c18b373
chernyshevdv <chernyshevdv@gmail.com>
2013-04-24 15:01:58 +0400

    ITASSETS-30

--- app/models/asset_stocktake_row.rb
+++ app/models/asset_stocktake_row.rb
@@ -1,6 +1,6 @@
 class AssetStocktakeRow < ActiveRecord::Base
   self.table_name = 'rowAssetStocktake'
-  attr_accessible :document, :asset_id, :status, :title, :model_id, :doc_id
+  attr_accessible :document, :asset_id, :status, :title, :model_id, :doc_id, :connected_to
 
   belongs_to :document, class_name: 'AssetStocktakeDocument', foreign_key: 'doc_id'
   belongs_to :article, foreign_key: 'model_id'
